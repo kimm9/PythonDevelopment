4233a47f7ca78b18b3edcad06b66ba462df91ea6
Matt Kim <matt.kim712@gmail.com>
2018-03-12 12:20:41 -0400

    updated

--- coInfo/coInfo/coin/admin.py
+++ coInfo/coInfo/coin/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from coin.models import Category, Page, Coin, Portfolio
 
+#Customize admin Model Forms
+class PageAdmin(admin.ModelAdmin):
+  list_display = ['title', 'category', 'url']
+
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
 admin.site.register(Coin)
 admin.site.register(Portfolio)
--- coInfo/coInfo/coin/views.py
+++ coInfo/coInfo/coin/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from coin.models import Category
 
 # Create your views here.
 def index(request):
+  # Get the Database for a list of All categories
+  #order the Categories in descending order by likes 
   context_dict = {'boldmessage': "this is coin message"}
   return render(request, 'coin/index.html', context=context_dict)
 
